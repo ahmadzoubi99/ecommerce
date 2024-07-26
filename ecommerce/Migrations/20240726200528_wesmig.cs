82f8cdeb77c09e1f435f01f4f3e1ef2ef0cb2709
WesalAlshamali <wesalmazen4@gmail.com>
2024-07-26 23:21:05 +0300

    wesal

---
+++ ecommerce/Migrations/20240726200528_wesmig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.Migrations
+{
+    public partial class wesmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
